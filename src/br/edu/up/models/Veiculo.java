72b8f08c50156054ae949cead25f5f8ee6954081
MarcelFlrs <127535882+MarcelFlrs@users.noreply.github.com>
2024-06-10 09:19:11 -0300

    Classe Veiculo

--- src/br/edu/up/models/Veiculo.java
+++ src/br/edu/up/models/Veiculo.java
@@ -4,4 +4,22 @@ public abstract class Veiculo {
 
     private String modelo;
     private String placa;
+    private String cor;
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
 }
