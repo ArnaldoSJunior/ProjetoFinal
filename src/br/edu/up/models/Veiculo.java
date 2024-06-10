a390e60d66aa12bd5c44405124c65ed9db5e2cd5
RafaGutowski <162761689+RafaGutowski@users.noreply.github.com>
2024-06-10 09:21:44 -0300

    Cor e Construtor Classe Veiculos

--- src/br/edu/up/models/Veiculo.java
+++ src/br/edu/up/models/Veiculo.java
@@ -6,6 +6,20 @@ public abstract class Veiculo {
     private String placa;
     private String cor;
 
+    public Veiculo(String modelo, String placa, String cor) {
+        this.modelo = modelo;
+        this.placa = placa;
+        this.cor = cor;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
     public String getModelo() {
         return modelo;
     }
