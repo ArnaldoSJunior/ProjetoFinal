b99b74e01f1362263c7b3074c416210631132e84
MarcelFlrs <127535882+MarcelFlrs@users.noreply.github.com>
2024-06-30 18:53:37 -0300

    Gerenciador de arquivos moto

--- src/br/edu/up/daos/GerenciadorDeArquivosMoto.java
+++ src/br/edu/up/daos/GerenciadorDeArquivosMoto.java
@@ -1,5 +1,50 @@
 package br.edu.up.daos;
 
-public class GerenciadorDeArquivosMoto {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+import br.edu.up.models.Moto;
+
+public class GerenciadorDeArquivosMoto {
+    private String header = "";
+    private String nomeDoArquivoCaminhonete = "G:\\java\\ProjetoFinal\\src\\br\\edu\\up\\motos.csv";
+
+    public List<Moto> getMoto(){
+        List<Moto> motos = new ArrayList<>();
+
+        try {
+
+            File arquivoLeituraMoto = new File(nomeDoArquivoCaminhonete);
+            Scanner leitorMoto = new Scanner(arquivoLeituraMoto);
+
+            if (leitorMoto.hasNextLine()) {
+                header = leitorMoto.nextLine();
+            }
+
+            while (leitorMoto.hasNextLine()) {
+                String linha = leitorMoto.nextLine();
+                String[] dados = linha.split(";");
+
+                String placa = dados[0];
+                String modelo = dados[1];
+                String cor = dados[2];
+
+              Moto moto = new Moto(modelo, placa, cor);
+                motos.add(moto);
+            }
+
+            leitorMoto.close();
+
+        } catch (FileNotFoundException e) {
+            
+        }
+
+        return motos;
+    }
 }
+    
+    
+    
