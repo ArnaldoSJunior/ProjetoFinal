8af6c7c5845d67b66caef0516a7ed2ac089bbe5b
MiguelViapiana <migvjung@gmail.com>
2024-07-01 08:56:21 -0300

    .

--- src/br/edu/up/controllers/ControleEstacionamento.java
+++ src/br/edu/up/controllers/ControleEstacionamento.java
@@ -12,6 +12,8 @@ public class ControleEstacionamento {
     Estacionamento est = new Estacionamento();
     private GerenciadorDeArquivosMensalista gArquivosMensalista = new GerenciadorDeArquivosMensalista();
     private GerenciadorDeArquivosCarro gArquivosCarro = new GerenciadorDeArquivosCarro();
+    private GerenciadorDeAquivosCaminhonete gArquivosCaminhonete = new GerenciadorDeAquivosCaminhonete();
+    private GerenciadorDeArquivosMoto gArquivosMoto = new GerenciadorDeArquivosMoto();
     private List<Mensalista> mensalistas;
     private List<Carro> carros;
     private List<Moto> motos;
@@ -20,6 +22,8 @@ public class ControleEstacionamento {
     public ControleEstacionamento() {
         this.mensalistas = gArquivosMensalista.getMensalista();
         this.carros = gArquivosCarro.getCarros();
+        this.motos = gArquivosMoto.getMoto();
+        this.caminhonetes = gArquivosCaminhonete.getCaminhonete();
     }
 
     public String criarMensalista(int op, String nome, String cpf, String telefone, String modelo, String placa,
@@ -113,6 +117,96 @@ public class ControleEstacionamento {
         return mensalistas;
     }
 
-    
+    public String registrarEntrada(String placa, String modelo, String cor, int tipo) {
+        int vagaDisponivel = est.encontrarVagaDisponivel();
+        if (vagaDisponivel != -1) {
+            if (validarPlaca(placa) == true) {
+                contEntrada++;
+                est.getVagas(vagaDisponivel).ocuparVaga(placa, modelo, cor, tipo);
+                return "OK";
+            } else {
+                return "null";
+            }
+        } else {
+            return "null";
+        }
+    }
+
+    public String registarSaida(String placa) {
+        for (int i = 0; i < 10; i++) {
+            if (est.verificarPlaca(placa) != null && est.verificarPlaca(placa).equals("1")) {
+                if (est.getVagas(i).getCarroPlaca() != null && est.getVagas(i).getCarroPlaca().equals(placa)) {
+                    est.getVagas(i).liberarVaga();
+                    return "OK"; // Retorna "OK" se a placa for encontrada e a vaga for liberada
+                }
+            }
+        }
+        return "OFF"; // Retorna "OFF" se a placa não for encontrada ou se a vaga não estiver ocupada
+                      // por esse veículo
+    }
+
+    public int contarVagasDisponiveis() {
+        int contador = 0;
+        for (Vaga vaga : est.getVagas()) {
+            if (!vaga.ocupada()) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    public int consultarVaga(String placa) {
+        for (int i = 0; i < 10; i++) {
+            if (est.getVagas(i).getCarroPlaca().equals(placa)) {
+                return est.getVagas(i).getNumero();
+            }
+
+        }
+        return 0;
+    }
+
+    public Double finalizarPeriodo() {
+        Double valorFinal = contEntrada * 5.00;
+        return valorFinal;
+    }
+
+    public Double finalizarPeriodoTarde() {
+        double valorFinal = (contEntrada + carrosSegundoPeriodo) * 5.00;
+        return valorFinal;
+    }
+
+    public int carrosSegundoPeriodo() {
+        this.carrosSegundoPeriodo = 10 - contarVagasDisponiveis();
+        return carrosSegundoPeriodo;
+    }
+
+    public double finalizarPeriodoNoite() {
+        double valorFinal = (contEntrada + carrosSegundoPeriodo) * 5.00;
+        return valorFinal;
+    }
+
+    public int getContEntrada() {
+        return contEntrada;
+    }
+
+    public int getCarrosSegundoPeriodo() {
+        return carrosSegundoPeriodo;
+    }
+
+    public void setContEntrada(int contEntrada) {
+        this.contEntrada = contEntrada;
+    }
+
+    public boolean validarPlaca(String placa) {
+        String[] partes = placa.split("-");
+        if (partes.length != 2 || partes[0].length() != 3) {
+            return false;
+        }
+        // Verifica se a segunda parte consiste apenas em dígitos ou letras
+        if (!partes[1].matches("[0-9a-zA-Z]+")) {
+            return false;
+        }
+        return true;
+    }
 
 }
